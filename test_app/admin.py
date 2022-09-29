62ce5809e9706395d87f41db1ed30bae011ed0a2
Rhogiel Andoy <theglitch0710@gmail.com>
2022-09-29 16:36:27 +0800

    models.py modification due to changes 2

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Profile, UsersAreaInfo, RegionCode, ProvincialCode, \
-    MunCityCode, BrgyCode, ProfileAttachments, Farmer
+    MunCityCode, BrgyCode, ProfileAttachments, Farmer, FarmerDependents, AreaCrop
 
 
 admin.site.register(Profile)
@@ -11,3 +11,5 @@ admin.site.register(MunCityCode)
 admin.site.register(BrgyCode)
 admin.site.register(ProfileAttachments)
 admin.site.register(Farmer)
+admin.site.register(FarmerDependents)
+admin.site.register(AreaCrop)
